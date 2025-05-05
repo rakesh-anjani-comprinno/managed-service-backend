b065600a83f79d751d147ab0e159a0f240f66b13
rakesh-anjani-comprinno <rakesh.anjani@comprinno.net>
2025-05-05 18:48:30 +0530

    intial message

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const jiraWorklogRoutes = require('./routes/jira-worklog.routes');
 
 // Middleware to parse JSON requests
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:4200', 'http://13.233.159.246'] 
+}))
 
 // Routes
 app.use('/api/worklog', jiraWorklogRoutes);
