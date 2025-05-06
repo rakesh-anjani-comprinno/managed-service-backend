06bf276ab774463164bb007c76b2ec2b9192bc47
rakesh-anjani-comprinno <rakesh.anjani@comprinno.net>
2025-05-06 13:25:31 +0530

    cors updated

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const jiraWorklogRoutes = require('./routes/jira-worklog.routes');
 // Middleware to parse JSON requests
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:4200', 'http://13.233.159.246'] 
+    origin: ['http://localhost:4200', 'http://13.233.159.246','http://managed-services.comprinno.net','https://managed-services.comprinno.net'] 
 }))
 
 // Routes
